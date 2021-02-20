a2e1ee67dc23db5e7d07df638ad19caa53653d0c
Chelsea Boling <cmboling@github.com>
2021-02-19 16:28:30 -0800

    Update libmypy.c

--- 00-HelloWorld/libmypy.c
+++ 00-HelloWorld/libmypy.c
@@ -4,3 +4,13 @@
 PyObject * hello(PyObject * self) {
 	return PyUnicode_FromFormat("Hello C extension!");
 }
+
+// The leftmost const has no effect here.
+const int square(const int x) {
+  return x * x;
+}
+
+// The const has no effect here, and can easily be mistaken for const char*.
+char* const id(char* s) {
+  return s;
+}
